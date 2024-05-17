a6de5e843f1e46089c8e904317f160e4107d47ca
veenacc <veena.chenicheri@gmail.com>
2024-05-17 15:43:57 -0400

    add methods to product.rb file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+  # write methods to change variables 
+  def is_discounted
+    if price <= 10
+      p true
+    else
+      p false
+    end
+  end
+
+  def tax(p)
+    return tax= (0.09 * p).round(2)
+  end
+
+  def total
+    t =tax(price)
+    total = price + t
+    p total
+  end
 end
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -8,3 +8,5 @@ json.image_url product.image_url
 json.description product.description
 json.created_at product.created_at
 json.updated_at product.updated_at
+json.is_discounted product.is_discounted
+json.total product.total

9067ff8166a3e6d94ab4ea2268bd949a1c7a72d6
veenacc <veena.chenicheri@gmail.com>
2024-06-18 14:22:29 -0400

    added has_many:through functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
   ########################################################
 
   def is_discounted
